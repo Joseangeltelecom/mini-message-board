194652709993a6e6f80f6e381eab99438acff0cd
Jose Angel <joseangeltelecom@gmail.com>
2021-11-29 00:29:44 -0500

    my first express-app

---
+++ routes/index.js
@@ -0,0 +1,40 @@
+var express = require("express")
+var router = express.Router()
+const distanceInWords = require("date-fns/formatDistanceToNow")
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+]
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", {
+    title: "Mini Messageboard",
+    messages: messages,
+    format: distanceInWords,
+  })
+})
+
+router.get("/new", function (req, res, next) {
+  res.render("form")
+})
+
+router.post("/new", function (req, res) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.author,
+    added: new Date(),
+  })
+  res.redirect("/")
+})
+
+module.exports = router
